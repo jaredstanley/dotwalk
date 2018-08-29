2bd8259e8d74e58f08af7711e18dc1039e271fda
Jared Stanley <jstanley@purestorage.com>
2018-08-29 10:11:36 -0700

    working now

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var _App = {
 //
   init: function(){
     _App.createNewCanvas();
+    _App.setup();
 
   },
   createNewCanvas:function(){
@@ -15,10 +16,18 @@ var _App = {
     c.setAttribute("width", 500);
     c.setAttribute("height", 400);
     document.body.appendChild(c);
-    // document.getElementById("body").prepend(c);
     _App.canvas = document.getElementById("canvas");
     _App.ctx = _App.canvas.getContext("2d");
-    // console.log(_App.canvas.width);
+    console.log("_App.canvas.width");
+  },
+  setup:function(){
+    console.log("setup");
+    _App.ctx.strokeStyle="#800800";
+    _App.ctx.lineWidth=10;
+    _App.ctx.beginPath();
+    _App.ctx.moveTo(110,222);
+    _App.ctx.lineTo(300,150);
+    _App.ctx.stroke();
   }
 }
 window.onload=function(){
